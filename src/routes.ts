04ace2028499db0990de5c96c9d51bfdc3e9f236
skgCos <51274191+skgCos@users.noreply.github.com>
2022-07-09 00:20:01 -0700

    Add data validation to routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,51 @@
 import express from "express";
+import {body, query, validationResult} from "express-validator";
 
-function dataPOSTHandler(req: express.Request, res: express.Response): void {
-    res.send("Handler data POST");
-}
+const dataPOSTHandler = [
+    // Validate
+    body("voltage").isFloat(),
+    body("timestamp").isInt(),
+    // Process
+    (req: express.Request, res: express.Response): void => {
+        const validationErrors = validationResult(req);
+        if(!validationErrors.isEmpty()) {
+            res.status(400).json({errors: validationErrors.array()});
+            return;
+        }
 
-function dataGETHandler(req: express.Request, res: express.Response): void {
-    res.send("Handler data GET");
-}
+        res.send("Handler data POST");
+    }
+];
 
-function averageGETHandler(req: express.Request, res: express.Response): void {
-    res.send("Handler average GET");
-}
+const dataGETHandler = [
+    // Validate
+    query("n").isInt(),
+    // Process
+    (req: express.Request, res: express.Response): void => {
+        const validationErrors = validationResult(req);
+        if(!validationErrors.isEmpty()) {
+            res.status(400).json({errors: validationErrors.array()});
+            return;
+        }
+
+        res.send("Handler data GET");
+    }
+];
+
+const averageGETHandler = [
+    // Validate
+    query("n").isInt(),
+    // Process
+    (req: express.Request, res: express.Response): void => {
+        const validationErrors = validationResult(req);
+        if(!validationErrors.isEmpty()) {
+            res.status(400).json({errors: validationErrors.array()});
+            return;
+        }
+
+        res.send("Handler average GET");
+    }
+];
 
 function wrongMethodHandler(req: express.Request, res: express.Response): void {
     res.status(405).send("Wrong method");
